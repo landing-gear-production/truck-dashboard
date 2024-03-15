f34d5f9ddf6a4bb12478fde5734bbf665bb7b8e2
dszhusb <71653499+dszhusb@users.noreply.github.com>
2024-03-15 15:18:42 -0400

    Implementing ESP32RET Library

---
+++ src/can_manager.cpp
@@ -0,0 +1,177 @@
+#include <Arduino.h>
+#include "can_manager.h"
+#include "esp32_can.h"
+#include "config.h"
+#include "SerialConsole.h"
+#include "gvret_comm.h"
+#include "lawicel.h"
+#include "ELM327_Emulator.h"
+
+CANManager::CANManager()
+{
+
+}
+
+void CANManager::setup()
+{
+    for (int i = 0; i < SysSettings.numBuses; i++)
+    {
+        if (settings.canSettings[i].enabled)
+        {
+            canBuses[i]->enable();
+            if ((settings.canSettings[i].fdMode == 0) || !canBuses[i]->supportsFDMode())
+            {
+                canBuses[i]->begin(settings.canSettings[i].nomSpeed, 255);
+                Serial.printf("Enabled CAN%u with speed %u\n", i, settings.canSettings[i].nomSpeed);
+                if ( (i == 0) && (settings.systemType == 2) )
+                {
+                  digitalWrite(SW_EN, HIGH); //MUST be HIGH to use CAN0 channel
+                  Serial.println("Enabling SWCAN Mode");
+                }
+                if ( (i == 1) && (settings.systemType == 2) )
+                {
+                  digitalWrite(SW_EN, LOW); //MUST be LOW to use CAN1 channel
+                  Serial.println("Enabling CAN1 will force CAN0 off.");
+                }
+            }
+            else
+            {
+                canBuses[i]->beginFD(settings.canSettings[i].nomSpeed, settings.canSettings[i].fdSpeed);
+                Serial.printf("Enabled CAN1 In FD Mode With Nominal Speed %u and Data Speed %u", 
+                                settings.canSettings[i].nomSpeed, settings.canSettings[i].fdSpeed);
+            }
+
+            if (settings.canSettings[i].listenOnly) 
+            {
+                canBuses[i]->setListenOnlyMode(true);
+            }
+            else
+            {
+                canBuses[i]->setListenOnlyMode(false);
+            }
+            canBuses[i]->watchFor();
+        } 
+        else
+        {
+            canBuses[i]->disable();
+        }
+    }
+
+    for (int j = 0; j < NUM_BUSES; j++)
+    {
+        busLoad[j].bitsPerQuarter = settings.canSettings[j].nomSpeed / 4;
+        busLoad[j].bitsSoFar = 0;
+        busLoad[j].busloadPercentage = 0;
+        if (busLoad[j].bitsPerQuarter == 0) busLoad[j].bitsPerQuarter = 125000;
+    }
+
+    busLoadTimer = millis();
+}
+
+void CANManager::addBits(int offset, CAN_FRAME &frame)
+{
+    if (offset < 0) return;
+    if (offset >= NUM_BUSES) return;
+    busLoad[offset].bitsSoFar += 41 + (frame.length * 9);
+    if (frame.extended) busLoad[offset].bitsSoFar += 18;
+}
+
+void CANManager::addBits(int offset, CAN_FRAME_FD &frame)
+{
+    if (offset < 0) return;
+    if (offset >= NUM_BUSES) return;
+    busLoad[offset].bitsSoFar += 41 + (frame.length * 9);
+    if (frame.extended) busLoad[offset].bitsSoFar += 18;
+}
+
+void CANManager::sendFrame(CAN_COMMON *bus, CAN_FRAME &frame)
+{
+    int whichBus = 0;
+    for (int i = 0; i < NUM_BUSES; i++) if (canBuses[i] == bus) whichBus = i;
+    bus->sendFrame(frame);
+    addBits(whichBus, frame);
+}
+
+void CANManager::sendFrame(CAN_COMMON *bus, CAN_FRAME_FD &frame)
+{
+    int whichBus = 0;
+    for (int i = 0; i < NUM_BUSES; i++) if (canBuses[i] == bus) whichBus = i;
+    bus->sendFrameFD(frame);
+    addBits(whichBus, frame);
+}
+
+
+void CANManager::displayFrame(CAN_FRAME &frame, int whichBus)
+{
+    if (settings.enableLawicel && SysSettings.lawicelMode) 
+    {
+        lawicel.sendFrameToBuffer(frame, whichBus);
+    } 
+    else 
+    {
+        if (SysSettings.isWifiActive) wifiGVRET.sendFrameToBuffer(frame, whichBus);
+        else serialGVRET.sendFrameToBuffer(frame, whichBus);
+    }
+}
+
+void CANManager::displayFrame(CAN_FRAME_FD &frame, int whichBus)
+{
+    if (settings.enableLawicel && SysSettings.lawicelMode) 
+    {
+        //lawicel.sendFrameToBuffer(frame, whichBus);
+    } 
+    else 
+    {
+        if (SysSettings.isWifiActive) wifiGVRET.sendFrameToBuffer(frame, whichBus);
+        else serialGVRET.sendFrameToBuffer(frame, whichBus);
+    }
+}
+
+void CANManager::loop()
+{
+    CAN_FRAME incoming;
+    CAN_FRAME_FD inFD;
+    size_t wifiLength = wifiGVRET.numAvailableBytes();
+    size_t serialLength = serialGVRET.numAvailableBytes();
+    size_t maxLength = (wifiLength > serialLength) ? wifiLength : serialLength;
+
+    if (millis() > (busLoadTimer + 250)) {
+        busLoadTimer = millis();
+        busLoad[0].busloadPercentage = ((busLoad[0].busloadPercentage * 3) + (((busLoad[0].bitsSoFar * 1000) / busLoad[0].bitsPerQuarter) / 10)) / 4;
+        //Force busload percentage to be at least 1% if any traffic exists at all. This forces the LED to light up for any traffic.
+        if (busLoad[0].busloadPercentage == 0 && busLoad[0].bitsSoFar > 0) busLoad[0].busloadPercentage = 1;
+        busLoad[0].bitsPerQuarter = settings.canSettings[0].nomSpeed / 4;
+        busLoad[0].bitsSoFar = 0;
+        if(busLoad[0].busloadPercentage > busLoad[1].busloadPercentage){
+            //updateBusloadLED(busLoad[0].busloadPercentage);
+        } else{
+            //updateBusloadLED(busLoad[1].busloadPercentage);
+        }
+    }
+
+    for (int i = 0; i < SysSettings.numBuses; i++)
+    {
+        if (!canBuses[i]) continue;
+        if (!settings.canSettings[i].enabled) continue;
+        while ( (canBuses[i]->available() > 0) && (maxLength < (WIFI_BUFF_SIZE - 80)))
+        {
+            if (settings.canSettings[i].fdMode == 0)
+            {
+                canBuses[i]->read(incoming);
+                addBits(i, incoming);
+                displayFrame(incoming, i);
+            }
+            else
+            {
+                canBuses[i]->readFD(inFD);
+                addBits(i, inFD);
+                displayFrame(inFD, i);
+            }
+            toggleRXLED();
+            if ( (incoming.id > 0x7DF && incoming.id < 0x7F0) || elmEmulator.getMonitorMode() ) elmEmulator.processCANReply(incoming);
+            wifiLength = wifiGVRET.numAvailableBytes();
+            serialLength = serialGVRET.numAvailableBytes();
+            maxLength = (wifiLength > serialLength) ? wifiLength:serialLength;
+        }
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,282 @@
-#include <Arduino.h>
+/*
+ ESP32RET.ino
 
-void setup() {
-    Serial.begin(115200);
+ Created: June 1, 2020
+ Author: Collin Kidder
+
+Copyright (c) 2014-2020 Collin Kidder, Michael Neuweiler
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include "config.h"
+#include <esp32_can.h>
+#include <SPI.h>
+#include <Preferences.h>
+// #include <FastLED.h>
+#include "ELM327_Emulator.h"
+#include "SerialConsole.h"
+#include "wifi_manager.h"
+#include "gvret_comm.h"
+#include "can_manager.h"
+#include "lawicel.h"
+#include "Arduino.h"
+
+byte i = 0;
+
+uint32_t lastFlushMicros = 0;
+
+bool markToggle[6];
+uint32_t lastMarkTrigger = 0;
+
+EEPROMSettings settings;
+SystemSettings SysSettings;
+Preferences nvPrefs;
+char deviceName[20];
+char otaHost[40];
+char otaFilename[100];
+
+uint8_t espChipRevision;
+
+ELM327Emu elmEmulator;
+
+WiFiManager wifiManager;
+
+GVRET_Comm_Handler serialGVRET; // gvret protocol over the serial to USB connection
+GVRET_Comm_Handler wifiGVRET;   // GVRET over the wifi telnet port
+CANManager canManager;          // keeps track of bus load and abstracts away some details of how things are done
+LAWICELHandler lawicel;
+
+SerialConsole console;
+
+// CRGB leds[A5_NUM_LEDS]; //A5 has the largest # of LEDs so use that one even for A0 or EVTV
+
+CAN_COMMON *canBuses[NUM_BUSES];
+
+// initializes all the system EEPROM values. Chances are this should be broken out a bit but
+// there is only one checksum check for all of them so it's simple to do it all here.
+void loadSettings()
+{
+    Logger::console("Loading settings....");
+
+    // Logger::console("%i\n", espChipRevision);
+
+    for (int i = 0; i < NUM_BUSES; i++)
+        canBuses[i] = nullptr;
+
+    nvPrefs.begin(PREF_NAME, false);
+
+    settings.useBinarySerialComm = nvPrefs.getBool("binarycomm", false);
+    settings.logLevel = nvPrefs.getUChar("loglevel", 1); // info
+    settings.wifiMode = nvPrefs.getUChar("wifiMode", 2); // Wifi defaults to creating an AP
+    settings.enableBT = nvPrefs.getBool("enable-bt", false);
+    settings.enableLawicel = nvPrefs.getBool("enableLawicel", true);
+    //    settings.systemType = nvPrefs.getUChar("systype", (espChipRevision > 2) ? 0 : 1); //0 = A0, 1 = EVTV ESP32
+    settings.systemType = nvPrefs.getUChar("systype", 0); // 0 = A0, 1 = EVTV ESP32
+
+    Logger::console("Running on RejsaCAN ESP32-S3");
+    canBuses[0] = &CAN0;
+    SysSettings.LED_CANTX = 10;
+    SysSettings.LED_CANRX = 11;
+    SysSettings.LED_LOGGING = 255;
+    SysSettings.LED_CONNECTION_STATUS = 255;
+    SysSettings.fancyLED = false;
+    SysSettings.logToggle = false;
+    SysSettings.txToggle = true;
+    SysSettings.rxToggle = true;
+    SysSettings.lawicelAutoPoll = false;
+    SysSettings.lawicelMode = false;
+    SysSettings.lawicellExtendedMode = false;
+    SysSettings.lawicelTimestamping = false;
+    SysSettings.numBuses = 1; // Currently we support CAN0
+    SysSettings.isWifiActive = true;
+    SysSettings.isWifiConnected = false;
+    strcpy(deviceName, REJSANAME);
+    strcpy(otaHost, "rejsa.nu");
+    strcpy(otaFilename, "/rejsacan/esp32-s3ret.bin");
+    CAN0.setCANPins(GPIO_NUM_18, GPIO_NUM_19);
+    // pinMode(SysSettings.LED_CANTX, OUTPUT);
+    // pinMode(SysSettings.LED_CANRX, OUTPUT);
+
+    if (nvPrefs.getString("SSID", settings.SSID, 32) == 0)
+    {
+        strcpy(settings.SSID, deviceName);
+        strcat(settings.SSID, "SSID");
+    }
+
+    if (nvPrefs.getString("wpa2Key", settings.WPA2Key, 64) == 0)
+    {
+        strcpy(settings.WPA2Key, "aBigSecret");
+    }
+
+    if (nvPrefs.getString("btname", settings.btName, 32) == 0)
+    {
+        strcpy(settings.btName, "ELM327-");
+        strcat(settings.btName, deviceName);
+    }
+
+    char buff[80];
+    for (int i = 0; i < SysSettings.numBuses; i++)
+    {
+        sprintf(buff, "can%ispeed", i);
+        settings.canSettings[i].nomSpeed = nvPrefs.getUInt(buff, 500000);
+        sprintf(buff, "can%i_en", i);
+        settings.canSettings[i].enabled = nvPrefs.getBool(buff, (i < 2) ? true : false);
+        sprintf(buff, "can%i-listenonly", i);
+        settings.canSettings[i].listenOnly = nvPrefs.getBool(buff, false);
+        sprintf(buff, "can%i-fdspeed", i);
+        settings.canSettings[i].fdSpeed = nvPrefs.getUInt(buff, 5000000);
+        sprintf(buff, "can%i-fdmode", i);
+        settings.canSettings[i].fdMode = nvPrefs.getBool(buff, false);
+    }
+
+    nvPrefs.end();
+
+    Logger::setLoglevel((Logger::LogLevel)settings.logLevel);
+
+    for (int rx = 0; rx < NUM_BUSES; rx++)
+        SysSettings.lawicelBusReception[rx] = true; // default to showing messages on RX
 }
 
-void loop() {
-    Serial.println("Hello Github");
-    delay(1000);
+void setup()
+{
+    // delay(5000); //just for testing. Don't use in production
+
+    espChipRevision = ESP.getChipRevision();
+
+    // Serial.begin(1000000); //for production
+    Serial.begin(115200); // for testing
+
+    SysSettings.isWifiConnected = false;
+
+    loadSettings();
+
+    // If you enable PSRAM then BluetoothSerial will kill everything via a heap error. These calls
+    // try to debug that. But, no dice yet. :(
+    // heap_caps_print_heap_info(MALLOC_CAP_8BIT);
+
+    /*
+        if (settings.enableBT)
+        {
+
+          Serial.println("Starting bluetooth");
+            elmEmulator.setup();
+            if (SysSettings.fancyLED && (settings.wifiMode == 0) )
+            {
+                leds[0] = CRGB::Green;
+                FastLED.show();
+            }
+        }
+    */
+    /*else*/ wifiManager.setup();
+
+    // heap_caps_print_heap_info(MALLOC_CAP_8BIT);
+
+    // heap_caps_print_heap_info(MALLOC_CAP_8BIT);
+
+    Serial.print("Build number: ");
+    Serial.println(CFG_BUILD_NUM);
+
+    canManager.setup();
+
+    SysSettings.lawicelMode = false;
+    SysSettings.lawicelAutoPoll = false;
+    SysSettings.lawicelTimestamping = false;
+    SysSettings.lawicelPollCounter = 0;
+
+    elmEmulator.setup();
+
+    Serial.print("Free heap after setup: ");
+    Serial.println(esp_get_free_heap_size());
+
+    Serial.print("Done with init\n");
+}
+
+/*
+Send a fake frame out USB and maybe to file to show where the mark was triggered at. The fake frame has bits 31 through 3
+set which can never happen in reality since frames are either 11 or 29 bit IDs. So, this is a sign that it is a mark frame
+and not a real frame. The bottom three bits specify which mark triggered.
+*/
+void sendMarkTriggered(int which)
+{
+    CAN_FRAME frame;
+    frame.id = 0xFFFFFFF8ull + which;
+    frame.extended = true;
+    frame.length = 0;
+    frame.rtr = 0;
+    canManager.displayFrame(frame, 0);
+}
+
+/*
+Loop executes as often as possible all the while interrupts fire in the background.
+The serial comm protocol is as follows:
+All commands start with 0xF1 this helps to synchronize if there were comm issues
+Then the next byte specifies which command this is.
+Then the command data bytes which are specific to the command
+Lastly, there is a checksum byte just to be sure there are no missed or duped bytes
+Any bytes between checksum and 0xF1 are thrown away
+
+Yes, this should probably have been done more neatly but this way is likely to be the
+fastest and safest with limited function calls
+*/
+void loop()
+{
+    // uint32_t temp32;
+    bool isConnected = false;
+    int serialCnt;
+    uint8_t in_byte;
+
+    /*if (Serial)*/ isConnected = true;
+
+    if (SysSettings.lawicelPollCounter > 0)
+        SysSettings.lawicelPollCounter--;
+    //}
+
+    canManager.loop();
+    /*if (!settings.enableBT)*/ wifiManager.loop();
+
+    size_t wifiLength = wifiGVRET.numAvailableBytes();
+    size_t serialLength = serialGVRET.numAvailableBytes();
+    size_t maxLength = (wifiLength > serialLength) ? wifiLength : serialLength;
+
+    // If the max time has passed or the buffer is almost filled then send buffered data out
+    if ((micros() - lastFlushMicros > SER_BUFF_FLUSH_INTERVAL) || (maxLength > (WIFI_BUFF_SIZE - 40)))
+    {
+        lastFlushMicros = micros();
+        if (serialLength > 0)
+        {
+            Serial.write(serialGVRET.getBufferedBytes(), serialLength);
+            serialGVRET.clearBufferedBytes();
+        }
+        if (wifiLength > 0)
+        {
+            wifiManager.sendBufferedData();
+        }
+    }
+
+    serialCnt = 0;
+    while ((Serial.available() > 0) && serialCnt < 128)
+    {
+        serialCnt++;
+        in_byte = Serial.read();
+        serialGVRET.processIncomingByte(in_byte);
+    }
+
+    elmEmulator.loop();
 }
