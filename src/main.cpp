3bdc4b03d93c406ef0469eb4dc4501ea5315d905
Nishanth Samala <hey@outlandnish.com>
2024-04-07 23:41:48 -0400

    c6 with idf

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,54 @@
-#include <Arduino.h>
 #include "main.h"
 
+void delay(uint32_t ms) {
+  vTaskDelay(ms / portTICK_PERIOD_MS);
+}
+
+unsigned long millis() {
+  return (unsigned long) (esp_timer_get_time() / 1000ULL);
+}
+
 void setup() {
-  Serial.begin(115200);
-  
-  xTaskCreatePinnedToCore(canTask, "canTask", 4096, NULL, 10, NULL, 1);
+  printf("Setup called\n");
+  /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+  uart_config_t uart_config = {
+      .baud_rate = 115200,
+      .data_bits = UART_DATA_8_BITS,
+      .parity    = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_1,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+      .rx_flow_ctrl_thresh = 122,
+      .source_clk = UART_SCLK_DEFAULT,
+  };
+  int intr_alloc_flags = 0;
+
+#if CONFIG_UART_ISR_IN_IRAM
+  intr_alloc_flags = ESP_INTR_FLAG_IRAM;
+#endif
+
+  uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags);
+  uart_param_config(UART_NUM_0, &uart_config);
+  uart_set_pin(UART_NUM_0, 16, 17, 0, 0);
+
+  xTaskCreate(canTask, "canTask", 4096, NULL, 10, NULL);
 }
 
 void loop() {
-  if (Serial.available()) {
-    std::string line = Serial.readString().c_str();
+  // Configure a temporary buffer for the incoming data
+  int reported_length = 0;
+  uart_get_buffered_data_len(UART_NUM_0, (size_t*)&reported_length);
+  uint8_t data[reported_length + 1];
+  int len = uart_read_bytes(UART_NUM_0, data, reported_length, 0);
+  if (len > 0) {
+    std::string line = (const char*)data;
     auto parts = split(line, ",");
-    if (parts.size() == 2) {
-      speed = std::atoi(parts[0].c_str());
-      rpm = std::atoi(parts[1].c_str());
+    if (parts.size() >= 2) {
+      speed = static_cast<uint16_t>(std::atoi(parts[0].c_str()));
+      rpm = static_cast<uint16_t>(std::atoi(parts[1].c_str()));
     }
   }
+  delay(50);
 }
 
 void setupCAN() {
@@ -58,33 +91,48 @@ void setupCAN() {
 
 void canTask(void* args) {
   setupCAN();
+  STARTUP_TIME = millis();
 
-  uint16_t scaledRpm = engineRpm * 8;
-  uint16_t scaledSpeed = speed * 256;
+  uint16_t scaledRpm, scaledSpeed;
+  unsigned long now = millis();
 
   for (;;) {
-    // oldFeedbackData = feedbackData;
-    twai_message_t message; 
+    now = millis();
+    twai_message_t message;
 
-    if (twai_receive_v2(can0, &message, 100) == ESP_OK) {
+    if (twai_receive_v2(can0, &message, 0) == ESP_OK) {
       receivingData = true;
 
-      J1939Header header = parseHeader(message.identifier);
-      switch (header.pgn) {
-        case 61444:
-          message[4] = HIGH(scaledRpm);
-          message[3] = LOW(scaledRpm);
-          twai_transmit_v2(can1, &message, 100);
-          break;
-        case 0xFEF1:
-          message[2] = HIGH(scaledSpeed);
-          message[1] = LOW(scaledSpeed);
-          twai_transmit_v2(can1, &message, 100);
-          break;
+      twai_transmit_v2(can1, &message, 0);
+      if (now - STARTUP_TIME >= MESSAGE_SKIP_TIME_AFTER_STARTUP){
+        scaledRpm = static_cast<uint16_t>(rpm) * 8;
+        scaledSpeed = static_cast<uint16_t>(speed) * 256;
+
+        if (message.identifier == 0x0CF00400) {
+          message.data[4] = HIGH(scaledRpm);
+          message.data[3] = LOW(scaledRpm);
+        } else if (message.identifier == 0x18fef100) {
+          message.data[2] = HIGH(scaledSpeed);
+          message.data[1] = LOW(scaledSpeed);
+        }
+
+        twai_transmit_v2(can1, &message, 0);
+      }
+      else {
+        twai_transmit_v2(can1, &message, 0);
       }
     }
+    delay(1);
+  }
 
-    delay(5);
+  vTaskDelete(NULL);
+}
+
+extern "C" void app_main(void)
+{
+  setup();
+  while(true) {
+    loop();
   }
 
   vTaskDelete(NULL);
--- src/main.h
+++ src/main.h
@@ -1,16 +1,31 @@
+#include <driver/gpio.h>
+#include <driver/uart.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <driver/twai.h>
+#include <esp_timer.h>
+#include <string.h>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include "j1939.h"
 
+#define LOG_LOCAL_LEVEL ESP_LOG_WARNING
+#include "esp_log.h"
+
+#define BUF_SIZE (1024)
+
 #define LOW(x) ((x) & 0xff)
 #define HIGH(x) ((x) >> 8)
 
-#define CAN0_TX GPIO_NUM_18
-#define CAN0_RX GPIO_NUM_19
+#define CAN0_TX GPIO_NUM_19
+#define CAN0_RX GPIO_NUM_18
 #define CAN1_TX GPIO_NUM_20
 #define CAN1_RX GPIO_NUM_21
+
+unsigned long STARTUP_TIME = 0;
+unsigned long MESSAGE_SKIP_TIME_AFTER_STARTUP = 5000;
+bool receivingData = false;
 
 twai_handle_t can0;
 twai_handle_t can1;
@@ -31,8 +46,8 @@ std::vector<std::string> split(std::string s, std::string delimiter) {
   return res;
 }
 
-int16_t speed;
-int16_t rpm;
+uint16_t speed;
+uint16_t rpm;
 
 void setupCAN();
 void canTask(void *args);
